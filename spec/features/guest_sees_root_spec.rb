7e75e5febc45d45d5da321fdce9085c9650616d7
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-06-05 16:26:03 -0600

    add feature test for root path as guest

---
+++ spec/features/guest_sees_root_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'as a guest on root page' do
+  it 'I can see a list of top ten links' do
+    create_list(:link, 10)
+
+    visit root_path
+
+    Link.most_hot.each do |link|
+      expect(page).to have_content "#{link.url}"
+    end
+  end
+end
